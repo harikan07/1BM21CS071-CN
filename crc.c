b5a9c138f76abb88fbbf93d715c1bf6fc1987cd2
harikan07 <117370825+harikan07@users.noreply.github.com>
2023-09-11 09:46:08 +0530

    Add files via upload

--- crc.c
+++ crc.c
@@ -1,47 +1,91 @@
+// Include headers
 #include <stdio.h>
 #include <string.h>
-
-// CRC-CCITT polynomial: x^16 + x^12 + x^5 + 1 (0x1021)
-#define CRC_POLY 0x1021
-
-// Function to calculate CRC-CCITT checksum
-unsigned short calculateCRC(const char *data, int length) {
-    unsigned short crc = 0xFFFF; // Initial value
-    for (int i = 0; i < length; i++) {
-        crc ^= (unsigned short)data[i] << 8;
-        for (int j = 0; j < 8; j++) {
-            if (crc & 0x8000)
-                crc = (crc << 1) ^ CRC_POLY;
-            else
-                crc <<= 1;
-        }
-    }
-    return crc;
+// length of the generator polynomial
+#define N strlen(gen_poly)
+// data to be transmitted and received
+char data[28];
+// CRC value
+char check_value[28];
+// generator polynomial
+char gen_poly[10];
+// variables
+int data_length, i, j;
+// function that performs XOR operation
+void XOR()
+{
+    // if both bits are the same, the output is 0
+    // if the bits are different the output is 1
+    for (j = 1; j < N; j++)
+        check_value[j] = ((check_value[j] == gen_poly[j]) ? '0' : '1');
+}
+// Function to check for errors on the receiver side
+void receiver()
+{
+    // get the received data
+    printf("Enter the received data: ");
+    scanf("%s", data);
+    printf("\n-----------------------------\n");
+    printf("Data received: %s", data);
+    // Cyclic Redundancy Check
+    crc();
+    // Check if the remainder is zero to find the error
+    for (i = 0; (i < N - 1) && (check_value[i] != '1'); i++)
+        ;
+    if (i < N - 1)
+        printf("\nError detected\n\n");
+    else
+        printf("\nNo error detected\n\n");
 }
 
-int main() {
-    char data[100]; // Replace with your actual data
-    printf("Enter data: ");
+void crc()
+{
+    // initializing check_value
+    for (i = 0; i < N; i++)
+        check_value[i] = data[i];
+    do
+    {
+        // check if the first bit is 1 and calls XOR function
+        if (check_value[0] == '1')
+            XOR();
+        // Move the bits by 1 position for the next computation
+        for (j = 0; j < N - 1; j++)
+            check_value[j] = check_value[j + 1];
+        // appending a bit from data
+        check_value[j] = data[i++];
+    } while (i <= data_length + N - 1);
+    // loop until the data ends
+}
+
+int main()
+{
+    // get the data to be transmitted
+    printf("\nEnter data to be transmitted: ");
     scanf("%s", data);
-
-    int dataLength = strlen(data);
-    unsigned short checksum = calculateCRC(data, dataLength);
-
-    printf("Calculated CRC: 0x%04X\n", checksum);
-
-    // Simulating error by changing a bit
-    // data[2] ^= 0x01; // Uncomment this line to introduce an error
-
-    // Verify the received data
-    unsigned short receivedChecksum;
-    printf("Enter received CRC: ");
-    scanf("%hx", &receivedChecksum);
-
-    if (receivedChecksum == checksum) {
-        printf("Data is error-free.\n");
-    } else {
-        printf("Data contains errors.\n");
-    }
-
+    printf("\n Enter the Generating polynomial: ");
+    // get the generator polynomial
+    scanf("%s", gen_poly);
+    // find the length of data
+    data_length = strlen(data);
+    // appending n-1 zeros to the data
+    for (i = data_length; i < data_length + N - 1; i++)
+        data[i] = '0';
+    printf("\n----------------------------------------");
+    // print the data with padded zeros
+    printf("\n Data padded with n-1 zeros : %s", data);
+    printf("\n----------------------------------------");
+    // Cyclic Redundancy Check
+    crc();
+    // print the computed check value
+    printf("\nCRC or Check value is : %s", check_value);
+    // Append data with check_value(CRC)
+    for (i = data_length; i < data_length + N - 1; i++)
+        data[i] = check_value[i - data_length];
+    printf("\n----------------------------------------");
+    // printing the final data to be sent
+    printf("\n Final data to be sent : %s", data);
+    printf("\n----------------------------------------\n");
+    // Calling the receiver function to check errors
+    receiver();
     return 0;
 }
